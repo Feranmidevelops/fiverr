533aefcfed2694da5562c51cb9dc787e939c1330
Feranmi OYETUNDE <f.oyetunde@tehcoop.com>
2025-12-03 10:09:34 +0100

    finished mock frontend design

---
+++ src/components/slide/Slide.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Slide.scss";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: slidesToShow,
+    slidesToScroll: arrowsScroll,
+    arrows: true,
+  };
+
+  return (
+    <div className="slide">
+      <div className="container">
+        <Slider {...settings}>
+          {children}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
